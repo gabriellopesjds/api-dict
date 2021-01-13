bc2a39ba7de56d3cda0512a63d6ce570048f7b8a
Gabriel Lopes <gabriel.silva@matera.com>
2021-01-13 14:23:00 -0200

    Starting project

---
+++ src/main/java/ApiDictApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ApiDictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDictApplication.class, args);
+	}
+}
